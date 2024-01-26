e82e0d7bcd11f0a1dfd9666e5d73d73c6f8f693b
Marcos Silva Gonzalez <85124562+socram174@users.noreply.github.com>
2024-01-26 15:56:14 -0300

    Nuevas imagenes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const nav = document.querySelector(".nav");
+const links = document.querySelectorAll("a");
+
+
 
 const boton = document.querySelector("button");
 
@@ -6,9 +9,15 @@ boton.addEventListener("click", () => {
 
     nav.classList.remove("closed");
     nav.classList.add("active");
+    links.forEach((link)=> {
+        link.classList.add("active");
+    })
 })
 
 nav.addEventListener("click",() => {
     nav.classList.remove("active");
     nav.classList.add("closed");
+    links.forEach((link)=> {
+        link.classList.remove("active");
+    })
 })
